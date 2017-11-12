18aae61caaa6c5c756e364e5dab50d7f7909b88a
Jason Godson <jason.godson@shopify.com>
2017-11-12 10:15:36 -0700

    Add copy button to script output section

--- src/components/ScriptOutput.js
+++ src/components/ScriptOutput.js
@@ -3,11 +3,30 @@ import { Layout, Card, TextField } from '@shopify/polaris';
 import styles from './ScriptOutput.css';
 
 export default class ScriptOutput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.textfieldId = 'ScriptOutput';
+
+    this.copyOutputCode = this.copyOutputCode.bind(this);
+  }
+
+  copyOutputCode() {
+    document.querySelector(`#${this.textfieldId}`).select();
+    document.execCommand('selectAll');
+    document.execCommand('copy');
+  }
+
   render() {
+    const copy = {
+      content: "Copy",
+      onAction: this.copyOutputCode
+    };
+
     return (
-      <Card title="Script code" sectioned>
+      <Card title="Script code" sectioned actions={[copy]}>
         <TextField
-          id="ScriptOutput"
+          id={this.textfieldId}
           multiline={10}
           readOnly
           value={this.props.output}
