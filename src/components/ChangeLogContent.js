1a0304c5861046f84f986fb04a69a87644273dc1
Jason Godson <jason.godson@shopify.com>
2018-03-14 17:39:39 -0600

    Forgot to do change log and version bump

--- src/components/ChangeLogContent.js
+++ src/components/ChangeLogContent.js
@@ -4,11 +4,13 @@ import { Link, Stack } from '@shopify/polaris';
 export default function ChangeLogContent({newVersion} = props) {
   return (
     <div>
-    <h3>Change Log</h3>
+      <h3>Change Log</h3>
 
-    <ul>
-      <li><strong>IMPORTANT</strong> Reverted the wording of "Conditionally Allow Discount Codes" to "Conditionally Reject Discount Codes". Changed the functionality to work as it was originally intended instead (reject code if conditions match). If you import your previous campaigns that used this, you may need to make some adjustments to accommodate. Setting up this campaign as reject instead of allow seems to be easier to set up in most cases.</li>
-    </ul>
+      <ul>
+      <li>Fixed Multi-select causing script errors for line item selector/qualifier</li>
+
+      <li>Other minor bug fixes/tweaks</li>
+      </ul>
 
       <div style={{paddingTop: '2rem'}}>
         <Stack distribution="trailing">
--- src/versions.js
+++ src/versions.js
@@ -1,4 +1,4 @@
 export default {
-  currentVersion: "0.6.0",
+  currentVersion: "0.6.1",
   minimumVersion: "0.1.0"
 }
