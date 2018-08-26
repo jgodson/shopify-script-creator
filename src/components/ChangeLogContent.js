500c1e66a555b6808bb1030e7e4107c510457f9d
Jason Godson <jason.godson@shopify.com>
2018-08-26 07:08:08 -0400

    Use version of CSS that matches polaris version

--- src/components/ChangeLogContent.js
+++ src/components/ChangeLogContent.js
@@ -7,9 +7,7 @@ export default function ChangeLogContent({newVersion} = props) {
       <h3 id="changelog">Change Log</h3>
 
       <ul>
-      <li>Fixed a <a href="https://github.com/jgodson/shopify-script-creator/issues/2" target="_blank" rel="noopener noreferrer">bug</a> in the Buy X Get X campaign where it would not split out items properly when less than full multiples were bought (eg: buy 10 get 10 would give you 11 free if you purchased 11, 12 free if you purchased 12, etc). If the fixing of this bug causes any issues for you, please let me know by submitting a bug on <a href="https://github.com/jgodson/shopify-script-creator/issues" target="_blank" rel="noopener noreferrer">GitHub</a> or using the <a href="https://docs.google.com/forms/d/e/1FAIpQLSdBHeVvdU92fc8vsqRuvx5uWuYQFsW8U3Co5HDIusH8YEH_VA/viewform" target="_blank" rel="noopener noreferrer">feedback form</a></li>
-
-      <li>Reduced spacing between title and tags to better align them with the option selection</li>
+      <li>Fixed issue where new version of CSS would cause dropdowns to display incorrectly</li>
       </ul>
 
       <div style={{paddingTop: '2rem'}}>
--- src/versions.js
+++ src/versions.js
@@ -1,4 +1,4 @@
 export default {
-  currentVersion: "0.9.2",
+  currentVersion: "0.9.3",
   minimumVersion: "0.1.0"
 }
