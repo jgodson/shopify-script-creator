fa2c26015459f40fb76ebd98eb2eb4cdbb4f061e
Jason Godson <jason.godson@shopify.com>
2019-06-22 10:21:02 -0400

    Update version and changelog content

--- src/components/ChangeLogContent.js
+++ src/components/ChangeLogContent.js
@@ -7,8 +7,10 @@ export default function ChangeLogContent({newVersion} = props) {
       <h3 id="changelog">Change Log</h3>
 
       <ul>
-        <li>20.0 - Removed some fallbacks that were in place for default arguments that are no longer used. If you have issues with an existing saved script, try re-selecting your current options in all selects and then generate the script again.</li>
-        <li>20.0 - Added the ability to Activate and Deactivate campagins. Deactivated campaigns will not be included in generated code.</li>
+        <li>20.0 - Removed some fallbacks that were in place for default arguments that are no longer used. If you have issues with an existing saved script, try re-selecting your current options in all selects and then generate the script again.</li><br/>
+        <li>20.0 - Added the ability to Activate and Deactivate campagins. Deactivated campaigns will not be included in generated code.</li><br/>
+        <li>21.0 - (BETA FEATURE) Added the ability to minify scripts. This can help with scripts that end up going over the character limit and will fit them within that limit. If you encouter a script where minification causes issues, please let me know via the <b>Leave feedback</b> link.</li><br/>
+        <li>21.0 - Added a visible character count and the maximum characters allowed within a script.</li>
       </ul>
 
       <div style={{paddingTop: '2rem'}}>
