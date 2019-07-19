2655cbad03948fc55aabdd1bbadbf03cb73ca03d
Jason Godson <jason.godson@shopify.com>
2019-07-18 22:53:19 -0400

    Revamp logic for tax discounts

--- src/components/ChangeLogContent.js
+++ src/components/ChangeLogContent.js
@@ -7,7 +7,8 @@ export default function ChangeLogContent({newVersion} = props) {
       <h3 id="changelog">Change Log</h3>
 
       <ul>
-        <li>Updated the max script length to the new supported length (24,576 characters)</li>
+        <li>0.22.1 - Updated the max script length to the new supported length (24,576 characters)</li>
+        <li>0.22.2 - Changed <b>Tax Discount</b> to properly account for items that were discounted in a previous campaign instead of causing a script error</li>
       </ul>
 
       <div style={{paddingTop: '2rem'}}>
