eee62607ce33f13e8130b047392941afbec0b013
Jason Godson <jason.godson@shopify.com>
2020-06-10 23:29:05 -0400

    Add DiscountCodePattenMatcher Campaign

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -1,4 +1,10 @@
 module TestHelper
+  class Cart
+    def self.get_total_quantity(line_items)
+      line_items.reduce(0) { |tot, cur| tot + cur.quantity }
+    end
+  end
+
   class NeverQualifier
     def match?(cart, selector = nil)
       false
@@ -32,6 +38,19 @@ module TestHelper
 
     def match?(line_item)
       @id == line_item.variant.product.id
+    end
+  end
+
+  class Discounts
+    class PercentageDiscount
+      def initialize(percent, message)
+        @discount = (100 - percent) / 100.0
+        @message = message
+      end
+
+      def apply(item)
+        item.change_line_price(item.line_price * @discount, message: @message)
+      end
     end
   end
 end
