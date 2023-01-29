f3f1c8d84eaa8b046ee54ad62e22ed23289fae6b
Jason Godson <jason_c_g@hotmail.com>
2023-01-29 09:11:20 -0700

    Release new version

--- src/components/ChangeLogContent.js
+++ src/components/ChangeLogContent.js
@@ -7,12 +7,13 @@ export default function ChangeLogContent({newVersion} = props) {
       <h3 id="changelog">Change Log</h3>
 
       <ul>
-        <li>Important fix for the recently added <b>Grouped Tiered Discount</b> Campaign that could cause an error when no tier was applicable</li>
+        <li><a target="_blank" rel="noreferrer noopener" href="https://github.com/jgodson/shopify-script-creator/pull/66">Fix</a> for an unexpected case when the line item was nil</li>
+        <li>New <b>Variant Title</b> Selector</li>
       </ul>
 
       <div style={{paddingTop: '2rem'}}>
         <Stack distribution="trailing">
-          <Link url="https://github.com/jgodson/shopify-script-creator/releases">
+          <Link url="https://github.com/jgodson/shopify-script-creator/releases" external>
             View previous releases
           </Link>
         </Stack>
--- src/versions.js
+++ src/versions.js
@@ -1,4 +1,4 @@
 export default {
-  currentVersion: "0.33.1",
+  currentVersion: "0.34.0",
   minimumVersion: "0.1.0",
 }
