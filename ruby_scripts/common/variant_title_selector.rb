9ab33ba7565801df51e46d60914fcfdbaa7d10fa
Jason Godson <jason_c_g@hotmail.com>
2023-01-29 09:50:36 -0700

    Release new version

--- ruby_scripts/common/variant_title_selector.rb
+++ ruby_scripts/common/variant_title_selector.rb
@@ -1,11 +1,11 @@
 class VariantTitleSelector < Selector
-    def initialize(match_type, variant_titles)
-      @invert = match_type == :not_one
-      @variant_titles = variant_titles.map { |title| title.downcase }
-    end
-  
-    def match?(line_item)
-      @invert ^ @variant_titles.include?(line_item.variant.title.downcase)
-    end
+  def initialize(match_type, variant_titles)
+    @invert = match_type == :not_one
+    @variant_titles = variant_titles.map { |title| title.downcase }
   end
+
+  def match?(line_item)
+    @invert ^ @variant_titles.include?(line_item.variant.title.downcase)
+  end
+end
   
--- src/scripts/common.js
+++ src/scripts/common.js
@@ -657,15 +657,15 @@ end`,
 
   VariantTitleSelector: `
 class VariantTitleSelector < Selector
-    def initialize(match_type, variant_titles)
-      @invert = match_type == :not_one
-      @variant_titles = variant_titles.map { |title| title.downcase }
-    end
-  
-    def match?(line_item)
-      @invert ^ @variant_titles.include?(line_item.variant.title.downcase)
-    end
-  end`,
+  def initialize(match_type, variant_titles)
+    @invert = match_type == :not_one
+    @variant_titles = variant_titles.map { |title| title.downcase }
+  end
+
+  def match?(line_item)
+    @invert ^ @variant_titles.include?(line_item.variant.title.downcase)
+  end
+end`,
 
   ZipCodeQualifier: `
 class ZipCodeQualifier < Qualifier
